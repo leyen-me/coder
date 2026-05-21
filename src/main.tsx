6fa8f5b8d775c2062c85fb56feabffefa3fc9a4d
leyen <672228275@qq.com>
2026-05-21 10:13:09 +0800

    feat(ui): 集成 shadcn/ui 与 Tailwind 设计系统
    安装 shadcn 组件、配置 Tailwind/CSS 变量，并更新 App 入口验证 UI 栈。

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </React.StrictMode>,
 );
